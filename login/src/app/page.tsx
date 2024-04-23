b2fc00fd86f9c7343efc729607e7c90539bb52fa
joaogcastro <110193153+joaogcastro@users.noreply.github.com>
2024-04-23 10:53:42 -0300

    Import fix

--- login/src/app/page.tsx
+++ login/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css"; // Importe o componente de login aqui
-import LoginForm from "./components/loginForm";
+import LoginForm from "./components/LoginForm";
 
 export default function Home() {
   return (
